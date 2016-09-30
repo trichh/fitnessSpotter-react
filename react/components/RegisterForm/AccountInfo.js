cc91217961dc44d28ceceda057e64b84cd46cb15
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-09-30 13:11:01 -0400

    Styled register page components

--- react/components/RegisterForm/AccountInfo.js
+++ react/components/RegisterForm/AccountInfo.js
@@ -3,19 +3,36 @@ import React from 'react';
 // Exporting the AccountInfo component to use in the RegisterForm component
 export default class AccountInfo extends React.Component {
   render() {
+    const accountInfoTitle = {
+      borderBottom: 'solid 1 white',
+      color: 'white',
+      fontSize: 25
+    }
+    const accountInfoLabel = {
+      color: 'white',
+      display: 'block',
+      fontSize: 23,
+      paddingBottom: 5
+    }
+    const accountInfoInput = {
+      borderRadius: 2,
+      height: 25,
+      marginBottom: 10,
+      width: 279
+    }
     return (
       <div>
-        <h3>Account info</h3>
-        <label>Email</label>
-        <input type="text" name="email" />
-        <label>Password</label>
-        <input type="password" name="password" />
-        <label>Your name/gym name</label>
-        <input type="text" name="name" />
-        <label>Profile picture</label>
+        <h3 style={accountInfoTitle}>Account info</h3>
+        <label style={accountInfoLabel}>Email</label>
+        <input style={accountInfoInput} type="text" name="email" />
+        <label style={accountInfoLabel}>Password</label>
+        <input style={accountInfoInput} type="password" name="password" />
+        <label style={accountInfoLabel}>Your name/gym name</label>
+        <input style={accountInfoInput} type="text" name="name" />
+        <label style={accountInfoLabel}>Profile picture</label>
         <input type="file" id="profilePhoto" name="picture"/>
-        <label>Phone number</label>
-        <input type="text" name="number" />
+        <label style={accountInfoLabel}>Phone number</label>
+        <input style={accountInfoInput} type="text" name="number" />
       </div>
     );
   }
